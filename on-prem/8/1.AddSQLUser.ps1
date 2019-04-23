abb3137f3f7b87f73413dd5e6e2ab8990b424753
Giovanni Ferrara <>
2019-04-23 15:50:04 +0200

    stupid script to add current user as admin to SQL Server - - as vm provisioning resets user

---
+++ on-prem/8/1.AddSQLUser.ps1
@@ -0,0 +1,11 @@
+﻿Param(
+    [Parameter(Mandatory=$True)][string]$SqlAdminUser = "sa",
+    [Parameter(Mandatory=$True)][string]$SqlAdminPwd
+)
+
+
+$cpath = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+Import-Module "$cpath\AddSQLUser.psm1" -Force
+#stupid script to add current user as admin to SQL Server - - vm provisioning reset user
+Create-SQL-Login $env:COMPUTERNAME\$env:UserName $env:COMPUTERNAME $SqlAdminUser $SqlAdminPwd
---
+++ on-prem/8/AddSQLUser.psm1
@@ -0,0 +1,57 @@
+﻿
+function SQL-Get-Server-Instance
+{
+    param (
+        [parameter(Mandatory = $true)][string] $DatabaseServer,
+        [parameter(Mandatory = $true)][string] $InstanceName
+    )
+
+    if (!$InstanceName -or $InstanceName -eq "" -or $InstanceName -eq "MSSQLSERVER")
+        { return $DatabaseServer }
+    else
+        { return "$DatabaseServer\$InstanceName" }
+}
+
+
+function Create-SQL-Login
+{
+    param (
+        [parameter(Mandatory = $true)][string] $loginName,
+        [parameter(Mandatory = $true)][string] $DatabaseServer,
+        [parameter(Mandatory = $true)][string] $SaUser = "sa",
+        [parameter(Mandatory = $true)][string] $SaPwd,
+        [parameter(Mandatory = $false)][string] $InstanceName = "MSSQLSERVER"
+    )
+
+    $sqlConnection = $null
+
+    try
+    {
+        $Error.Clear()
+
+        $ServerInstance = SQL-Get-Server-Instance $DatabaseServer $InstanceName
+        $sqlConnection = New-Object System.Data.SqlClient.SqlConnection
+        $sqlConnection.ConnectionString = "Password=$SaPwd;User ID=$SaUser;Server=$ServerInstance"
+
+        $Command = New-Object System.Data.SqlClient.SqlCommand
+        $Command.CommandType = 1
+        $Command.Connection = $sqlConnection
+        $Command.CommandText = "create login [$loginName] from windows with default_database=[master], default_language=[us_english]"
+        $sqlConnection.Open()
+        $Command.ExecuteNonQuery() | Out-Null
+        $Command.CommandText = "exec master..sp_addsrvrolemember @loginame = N'$loginName', @rolename = N'sysadmin'"
+        $Command.ExecuteNonQuery() | Out-Null
+    }
+
+    catch
+    {
+        $str = (([string] $Error).Split(':'))[1]
+        <# Write-Error ($str.Replace('"', '')) #>
+    }
+
+    finally
+    {
+        if ($sqlConnection)
+            { $sqlConnection.Close() }
+    }
+}
