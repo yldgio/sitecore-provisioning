09d5a63e40e7e52226272bdc2895e28b4fdeaeea
Giovanni Ferrara <>
2019-04-23 15:23:20 +0200

    initial import

---
+++ 8/Prerequisites/Install-All.ps1
@@ -0,0 +1,17 @@
+function Install-Prerequisites {
+    $tasks=$(
+        '.\Install-Chocolatey.ps1',
+        '.\Install-IIS.ps1',
+        '.\Install-ChocoPackages.ps1',
+        '.\Install-mongodb.ps1'
+    )
+    foreach($task in $tasks){
+        Write-Host "=========================[START]$task=============================" -foreground Green
+        Invoke-Expression $task
+        Write-Host "=========================[DONE]$task==============================" -foreground Green
+    }
+}
+
+$Global:ProgressPreference = 'SilentlyContinue'
+Install-Prerequisites
+$Global:ProgressPreference = 'Continue'
